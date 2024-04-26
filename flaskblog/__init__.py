f107015958a6715e5ac76339dd02dbc146b7456e
Triad01 <luckypee01@gmail.com>
2024-04-26 05:28:50 +0100

    added custom error page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -26,15 +26,17 @@ def create_app(config_class=Config):
     login_manager.init_app(app)
     mail.init_app(app)
 
-    # Blueprints
+    # importing Blueprints
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
 
     # Register blueprints
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
 
